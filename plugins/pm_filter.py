f352a4baf6155d8d5a5734a811eb5b4d7ac24b24
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-02-13 10:40:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -416,8 +416,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             chat_id=query.from_user.id,
             file_id=file_id,
             caption=f_caption,
-            protect_content=True if ident == 'checksubp' else False
-        )
+            reply_markup=InlineKeyboardMarkup(buttons)
+            )
     elif query.data == "pages":
         await query.answer()
     elif query.data == "start":
