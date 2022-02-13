77948d5ad7c5e0a696652078ffe70298646e3b3b
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-02-13 12:04:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -422,15 +422,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons =  [[[
-            InlineKeyboardButton('🔰𝑎𝑑𝑑 𝑚𝑒 𝑡𝑜 𝑦𝑜𝑢𝑟 𝑐𝒉𝑎𝑡🔰', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('𝑎𝑑𝑑 𝑚𝑒 𝑡𝑜 𝑦𝑜𝑢𝑟 𝑐𝒉𝑎𝑡', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔰𝒉𝑒𝑙𝑝🔰', callback_data='help'),
-            InlineKeyboardButton('🔰𝑎𝑏𝑜𝑢𝑡🔰', callback_data='about')
+            InlineKeyboardButton('𝒉𝑒𝑙𝑝', callback_data='help'),
+            InlineKeyboardButton('𝑎𝑏𝑜𝑢𝑡', callback_data='about')
             ],[
             InlineKeyboardButton('🔍𝑠𝑒𝑎𝑟𝑐𝒉 𝒉𝑒𝑟𝑒 𝑚𝑜𝑣𝑖𝑒🔎', switch_inline_query_current_chat='')
             ],[
-            InlineKeyboardButton('🔰𝐷𝑒𝑣𝑒𝑙𝑜𝑝𝑒𝑟🔰', url='https://t.me/FILMS_BOT_PANTHER'),
-            InlineKeyboardButton('🔰𝐺𝑟𝑜𝑢𝑝🔰', url='https://t.me/BT_MOVIES_HD')
+            InlineKeyboardButton('𝐷𝑒𝑣𝑒𝑙𝑜𝑝𝑒𝑟', url='https://t.me/FILMS_BOT_PANTHER'),
+            InlineKeyboardButton('𝐺𝑟𝑜𝑢𝑝', url='https://t.me/BT_MOVIES_HD')
             ],[
             InlineKeyboardButton('✗ 𝐶𝑙𝑜𝑠𝑒 𝑇𝒉𝑒 𝑀𝑒𝑛𝑢 ✗', callback_data='close_data')
         ]]
