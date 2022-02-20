afaa077f182ca40e5b74c4dc22561d07e8f16aed
Zafarbarlaskar <96040383+Zafarbarlaskar@users.noreply.github.com>
2022-02-20 16:44:45 +0530

    Update corona.py

--- plugins/corona.py
+++ plugins/corona.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API = "https://api.sumanjay.cf/covid/?country="
 
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join our Channel ⚙', url='https://t.me/BT_MOVIES_HD')]])
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join our Channel ⚙', url='https://t.me/Moviescorner0205116')]])
 
 @Client.on_message(filters.command("covid"))
 async def reply_info(client, message):
