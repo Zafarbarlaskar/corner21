5bd48bc91e62acbf14c6fbd57826e2e3d2e8c478
Zafarbarlaskar <96040383+Zafarbarlaskar@users.noreply.github.com>
2022-02-28 17:50:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -903,7 +903,7 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply(sticker="I couldn't find anything related to that. Check your spelling"),
+    await msg.reply sticker=("I couldn't find anything related to that. Check your spelling"),
     reply_markup=InlineKeyboardMarkup(btn))
 
 
